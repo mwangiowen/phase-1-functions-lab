bd74143be54441c7e85cf8f2f18dad7a165c1dde
mwangiowennjugunasmith <owen.njugun@student.moringa.com>
2023-09-23 22:39:55 +0300

    completeD

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+    const hqLocation = 42; 
+    return Math.abs(blockNumber - hqLocation); 
+  }
+function distanceFromHqInFeet(blockNumber) {
+    const blocks = distanceFromHqInBlocks(blockNumber);
+    const feetPerBlock = 264; 
+    return blocks * feetPerBlock;
+}
+function distanceTravelledInFeet(start, destination) {
+    const feetInBlock = 264; 
+    const distanceInBlocks = Math.abs(destination - start); 
+    return distanceInBlocks * feetInBlock; 
+}
+function calculatesFarePrice(startBlock, endBlock) {
+      const feetTravelled = distanceTravelledInFeet(startBlock, endBlock);
+      return calculatedFare;
+}
+function calculatesFarePrice(startBlock, endBlock) {
+    const feetTravelled = distanceTravelledInFeet(startBlock, endBlock);
+      let calculatedFare;
+      if (feetTravelled <= 400) { 
+          return 0; 
+          } else if (feetTravelled > 400 && feetTravelled <= 2000) { 
+          return (feetTravelled - 400) * 0.02; 
+          } else if (feetTravelled > 2000 && feetTravelled <= 2500) { 
+          return 25 ; 
+          } else {
+        calculatedFare = "cannot travel that far"; 
+      }
+       return calculatedFare;
+}
+
+
+
+
+
+
